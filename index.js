6e5536f5d1698e3af8857611b1dde36933cea65a
Lmwet <lea.colson@gmail.com>
2020-03-24 12:50:19 +0100

    added codes from morning encounter: upload files, multer, uidSafe and form + route for /upload

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function(req, file, callback) {
+        uidSafe(24).then(function(uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    }
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152
+    }
+});
+
+//////END MULTER NOILERPLATE /////////
+
 const express = require("express");
 const app = express();
 const db = require("./utils/db.js");
 app.use(express.static("public")); // by default will look for a index.js file
-
-// let images = [
-//     {
-//         big: "Berlin",
-//         country: "DE"
-//     },
-//     {
-//         big: "Guayaquil",
-//         country: "Ecuador"
-//     },
-//     {
-//         big: "Ankara",
-//         country: "Turkey"
-//     },
-//     {
-//         big: "Bogotá",
-//         country: "Colombia"
-//     }
-// ];
 
 app.get("/images", (req, res) => {
     console.log("/images route has been hit!");
@@ -30,6 +35,24 @@ app.get("/images", (req, res) => {
             console.log("images in renderImages", images);
         })
         .catch(e => console.log("eror in renderImages", e));
+});
+
+app.post("/upload", uploader.single("file"), (req, res) => {
+    // gives you access to your file
+    console.log("file: ", req.file);
+    // gives you access to the user input
+    console.log("user input: ", req.body);
+
+    if (req.file) {
+        // you'll eventually want to make a db insert here for all the info!
+        res.json({
+            success: true
+        });
+    } else {
+        res.json({
+            success: false
+        });
+    }
 });
 
 app.listen(8080, () => console.log("listening from 8080"));
--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,55 @@
-console.log("linked");
-
 (function() {
     new Vue({
         el: "#main",
         data: {
             name: "Daily Sketch", //any info that we want to be updated instantly
-            images: []
-        }, // !!! COMA closes data
+            images: [],
+            title: "",
+            description: "",
+            username: "",
+            file: null
+        }, // watch out: COMA  data
 
         mounted: function() {
             console.log("mounting?");
 
             var self = this;
             axios.get("/images").then(function(response) {
-                console.log("response from /images: ", response.data);
+                console.log("response from /images: ", response.data.rows);
 
-                self.images = response.data;
+                self.images = response.data.rows;
                 console.log("self.images ", self.images);
             });
-        }
+        },
 
-        // methods: {
-        //     myFunction: function() {
-        //         console.log("bei mir laufts");
-        //     } //myFunction
-        // } //methods
+        methods: {
+            handleClick: function(e) {
+                e.preventDefault();
+                // 'this' allows me to see all the properties of data
+                console.log("this! ", this);
+
+                // we NEED to use FormData to send a file to the server
+                var formData = new FormData();
+                formData.append("title", this.title);
+                formData.append("description", this.description);
+                formData.append("username", this.username);
+                formData.append("file", this.file);
+
+                axios
+                    .post("/upload", formData)
+                    .then(function(resp) {
+                        console.log("resp from POST /uplaod: ", resp);
+                    })
+                    .catch(function(err) {
+                        console.log("err in POST /upload: ", err);
+                    });
+            },
+
+            handleChange: function(e) {
+                // console.log('handleChange is running!!!!');
+                // console.log('file: ', e.target.files[0]);
+                this.file = e.target.files[0];
+            }
+        }
     });
 })();
